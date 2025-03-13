6e834ec4457c7115d7ebad71b03066ee0a2b4f80
zalewskikamil <zalewskikamil89@gmail.com>
2025-03-13 23:35:19 +0100

    initial commit

---
+++ src/test/java/com/github/lpgflow/LpgFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lpgflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LpgFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
